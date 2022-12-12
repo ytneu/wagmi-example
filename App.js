1595d12914f54103d65eab8f354a5f02f3c0457b
ytneu <ytneuu@gmail.com>
2022-12-12 11:45:11 +0100

    add another chart

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { LineChart } from "react-native-wagmi-charts";
+import { CandlestickChart } from "react-native-wagmi-charts";
 
 const data = [
   {
     timestamp: 1625945400000,
-    value: 33575.25,
+    open: 33575.25,
+    high: 33600.52,
+    low: 33475.12,
+    close: 33520.11,
   },
   {
     timestamp: 1625946300000,
-    value: 33545.25,
+    open: 33545.25,
+    high: 33560.52,
+    low: 33510.12,
+    close: 33520.11,
   },
   {
     timestamp: 1625947200000,
-    value: 33510.25,
+    open: 33510.25,
+    high: 33515.52,
+    low: 33250.12,
+    close: 33250.11,
   },
   {
     timestamp: 1625948100000,
-    value: 33215.25,
+    open: 33215.25,
+    high: 33430.52,
+    low: 33215.12,
+    close: 33420.11,
   },
 ];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LineChart.Provider data={data}>
-        <LineChart>
-          <LineChart.Path />
-        </LineChart>
-      </LineChart.Provider>
+      <CandlestickChart.Provider data={data}>
+        <CandlestickChart>
+          <CandlestickChart.Candles />
+        </CandlestickChart>
+      </CandlestickChart.Provider>
     </View>
   );
 }
